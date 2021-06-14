c6e6443d23a80c87f82731e4ef6e862ee3adb0da
Rhisiart ap Gwilym <rhisiart.ap.gwilym@mobilise.cloud>
2021-06-14 17:19:05 +0100

    Refactor tests

--- CourseSearchAPIHttpTrigger/tests/test_courses_by_subject.py
+++ CourseSearchAPIHttpTrigger/tests/test_courses_by_subject.py
@@ -12,114 +12,25 @@ from course_to_label_mapper import CourseToLabelMapper
 from courses_by_subject import CoursesBySubject
 from courses_by_subject import build_course
 
-MAPPING_FILE = f'{PARENT_DIR}/fixtures/subjects-sort-by.json'
+MAPPING_DIR = f'{PARENT_DIR}/fixtures'
 FIXTURES_DIR = f'{CURRENT_DIR}/fixtures/courses_by_subject'
 
 
 class TestCoursesBySubject(unittest.TestCase):
 
-    # def setUp(self):
-    # "Hook method for setting up the test fixture before exercising it."
-    # pass
-
-
-    def test_build_course_using_english_language(self):
-        # ARRANGE
-        with open(f'{FIXTURES_DIR}/build_course_en_input.json', 'r') as file:
-            input = file.read()
-        input = json.loads(input)
-
-        with open(f'{FIXTURES_DIR}/build_course_en_output.json', 'r') as file:
-            output = file.read()
-        expected = json.loads(output)
-        
-        # ACT
-        actual = build_course(input["course"], input["institution"], input["language"])
-
-        # ASSERT
-        self.assertEqual(actual, expected)
-
-
-    def test_build_course_using_welsh_language(self):
-        # ARRANGE
-        with open(f'{FIXTURES_DIR}/build_course_cy_input.json', 'r') as file:
-            input = file.read()
-        input = json.loads(input)
-
-        with open(f'{FIXTURES_DIR}/build_course_cy_output.json', 'r') as file:
-            output = file.read()
-        expected = json.loads(output)
-
-        # ACT
-        actual = build_course(input["course"], input["institution"], input["language"])
-
-        # ASSERT
-        self.assertEqual(actual, expected)
-
-
-    # TODO do the same for law
-    def test_when_welsh_course_queried(self):
-        # ARRANGE
-        with open(f'{MAPPING_FILE}', 'r') as file:
-            mapping = file.read()
-        course_to_label_mapping = json.loads(mapping)
-
-        with open(f'{FIXTURES_DIR}/input.json', 'r') as file:
-            input = file.read()
-        courses = json.loads(input)        
-
-        counts = {'institutions': 131, 'courses': 716}
-        limit = "5000"
-        offset = "0"
-        language = "en"
-        queried_course_title = "welsh"
-
-        mapper = CourseToLabelMapper(course_to_label_mapping)
-        courseBySubject = CoursesBySubject(mapper)
-
-        # ACT
-        actual = courseBySubject.group(queried_course_title,
-                                       courses,
-                                       counts, 
-                                       int(limit),
-                                       int(offset), 
-                                       language,
-                                       )
-
-        # ASSERT
-        items = actual["items"]
-        self.assertEqual(len(items.keys()), 2)
-
-
     def test_when_marketing_course_queried(self):
         # ARRANGE
-        with open(f'{MAPPING_FILE}', 'r') as file:
-            mapping = file.read()
-        course_to_label_mapping = json.loads(mapping)
-
-        with open(f'{FIXTURES_DIR}/input.json', 'r') as file:
-            input = file.read()
-        courses = json.loads(input)
-
-        with open(f'{FIXTURES_DIR}/output.json', 'r') as file:
-            output = file.read()
-        expected = json.loads(output)
-
-        counts = {'institutions': 131, 'courses': 716}
-        limit = "5000"
-        offset = "0"
-        language = "en"
+        courses = self.load_fixture('input.json')
+        expected = self.load_fixture('output.json')
         queried_course_title = "marketing"
-
-        mapper = CourseToLabelMapper(course_to_label_mapping)
-        courseBySubject = CoursesBySubject(mapper)
+        language = "en"
 
         # ACT
-        actual = courseBySubject.group(queried_course_title,
+        actual = self.courseBySubject.group(queried_course_title,
                                        courses,
-                                       counts, 
-                                       int(limit),
-                                       int(offset), 
+                                       self.counts, 
+                                       int(self.limit),
+                                       int(self.offset), 
                                        language,
                                        )
 
@@ -175,3 +86,66 @@ class TestCoursesBySubject(unittest.TestCase):
 
         self.assertEqual(actual, expected)
 
+
+    # TODO do the same for law
+    def test_when_welsh_course_queried(self):
+        # ARRANGE
+        courses = self.load_fixture('input.json')
+        queried_course_title = "welsh"
+        language = "en"
+
+        # ACT
+        actual = self.courseBySubject.group(queried_course_title,
+                                       courses,
+                                       self.counts, 
+                                       int(self.limit),
+                                       int(self.offset), 
+                                       language,
+                                       )
+
+        # ASSERT
+        items = actual["items"]
+        self.assertEqual(len(items.keys()), 2)
+
+
+    def test_build_course_using_english_language(self):
+        self.buld_course('build_course_en_input.json', 'build_course_en_output.json')
+
+
+    def test_build_course_using_welsh_language(self):
+        self.buld_course('build_course_cy_input.json', 'build_course_cy_output.json')
+
+
+    def buld_course(self, filename_input, filename_expected):
+        # ARRANGE
+        input = self.load_fixture(filename_input)
+        expected = self.load_fixture(filename_expected)
+        
+        # ACT
+        actual = build_course(input["course"], input["institution"], input["language"])
+
+        # ASSERT
+        self.assertEqual(actual, expected)
+
+
+    def load_fixture(self, filename):
+        return self.load_file(FIXTURES_DIR, filename)
+
+
+    def load_mappings(self):
+        return self.load_file(f'{MAPPING_DIR}', 'subjects-sort-by.json')
+
+
+    def load_file(self, dir, filename):
+        with open(f'{dir}/{filename}', 'r') as file:
+            input = file.read()
+        return json.loads(input)
+
+
+    def setUp(self):
+        self.counts = {'institutions': 131, 'courses': 716}
+        self.limit = "5000"
+        self.offset = "0"
+        mappings = self.load_mappings()
+        mapper = CourseToLabelMapper(mappings)
+        self.courseBySubject = CoursesBySubject(mapper)
